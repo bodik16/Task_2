1f05f730e5fcd6512632a27410b4e464bdfb8825
Valentina <valya.romanchuk3211@gmail.com>
2018-11-01 13:16:44 +0200

    new class Hexagone

---
+++ Task_2_WPF/Task_2_WPF/Hexagone.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_2_WPF
+{
+    using System;
+    using System.Windows.Media;
+    using System.Windows.Shapes;
+
+    class Hexagone
+    {
+        private readonly int VERTICES_NUMBER = 6;
+        private PointCollection points;
+        public PointCollection Points
+        {
+            get
+            {
+                return points;
+            }
+            set
+            {
+                if (value.Count != VERTICES_NUMBER)
+                {
+                    throw new ArgumentException("Number of points must be 6");
+                }
+                points = value;
+            }
+        }
+        public Color HexagoneColor { get; set; }
+
+        public Hexagone() { }
+
+        public Hexagone(Polygon figure)
+        {
+            Points = figure.Points;
+            HexagoneColor = (figure.Fill as SolidColorBrush).Color;
+        }
+    }
+}
